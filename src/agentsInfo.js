80a69943381d387f0e1c190015b1b818a96868fc
Eloi Buil Cuadrat <eloixxl2008@gmail.com>
2025-04-14 20:01:22 +0200

    Reestructuring the agents resources directory + Adding "at_phone" image to all agents + Agents list is generated from the resources/agents directory automatically

--- src/agentsInfo.js
+++ src/agentsInfo.js
@@ -1,24 +1,32 @@
 import path from "path"
 import { __rootdirname } from "#src/dirnames.js";
+import { ensureDirExists } from "./lib/fileUtils.js";
 
-export const audioSamplesDir = path.join(__rootdirname, "/resources/agent_audio_samples")
-export const refTextsDir = path.join(__rootdirname, "/resources/agent_reference_texts")
+export const agentsDir = path.join(__rootdirname, "/resources/agents")
 
 export const agentsInfo = (() => {
-  const initialObj = {
-    "carles_puigdemont": {},
-    "pere_aragones": {},
-    "pedro_sanchez": {},
-    "mariano_rajoy": {},
-    "pablo_iglesias": {},
-    "santiago_abascal": {},
-    "francisco_franco": {},
-  }
+  const initialObj = {}
 
-  for (const agent in initialObj) {
-    initialObj[agent].audioSamplePathName = path.join(audioSamplesDir, `${agent}_audio_sample.mp3`)
-    initialObj[agent].referenceTextPathName = path.join(refTextsDir, `${agent}_reference_text.txt`)
-  }
+  const agentsDirs = fs.readdirSync(agentsDir)
+  agentsDirs.forEach((agentDir) => {
+    const agentPath = path.join(agentsDir, agentDir)
+    if (!fs.statSync(agentPath).isDirectory()) return
+    const agentName = path.parse(agentDir).name
+    initialObj[agentName] = {}
+
+    initialObj[agentName].audioSamplePathName = path.join(agentPath, `audio_sample.mp3`)
+    initialObj[agentName].referenceTextPathName = path.join(agentPath, `reference_text.txt`)
+    const agentImagesDir = path.join(agentPath, "images")
+    ensureDirExists(agentImagesDir)
+    initialObj[agentName].images = {}
+    fs.readdirSync(agentImagesDir).forEach((file) => {
+      const imagePath = path.join(agentImagesDir, file)
+      if(!fs.statSync(imagePath).isFile()) return
+      
+      const fileName = path.parse(file).name
+      initialObj[agentName].images[fileName] = imagePath
+    })
+  })
 
   return initialObj;
 })();
