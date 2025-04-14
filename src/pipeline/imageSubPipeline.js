dd8c13ffb53fc7a75fb461cf14863d0112f3eb1f
Eloi Buil Cuadrat <eloixxl2008@gmail.com>
2025-04-14 20:11:53 +0200

    Refactored image creation code --> split in multiple files

--- src/pipeline/imageSubPipeline.js
+++ src/pipeline/imageSubPipeline.js
@@ -1,35 +1,6 @@
 import path from "path"
 import fs from "fs"
-import { JSDOM } from "jsdom"
-import { openai } from "#src/apiClients/openai.js"
-
-const fetchWebImageUrl = async (prompt) => {
-    const searchUrl = new URL("https://www.google.com/search")
-        searchUrl.searchParams.append("udm", 2) // set to image search
-        searchUrl.searchParams.append("as_q", prompt) // search query
-        searchUrl.searchParams.append("imgsz", "xga") // image size = 1024x768
-        
-        const response = await fetch(searchUrl.href);
-        const html = await response.text();
-
-        // get 1st image url from html
-        const dom = new JSDOM(html);
-        const document = dom.window.document
-        const imageUrl = (new URL(document.querySelector("#rso > div > div > div.wH6SXe.u32vCb > div > div > div:nth-child(1) > div.czzyk.XOEbc > h3 > a").href)).searchParams.get("imgurl");
-
-        return imageUrl
-}
-
-const fetchAiImageUrl = async (prompt) => {
-    const response = await openai.images.generate({
-        model: "dall-e-3",
-        prompt,
-        n: 1,
-        size: "1024x1024",
-    });
-
-    return response.data[0].url // image url
-}
+import { fetchWebImageUrl, fetchAiImageUrl } from "./imageCreation.js"
 
 const imageUrlFetchers = {
     web: fetchWebImageUrl,
