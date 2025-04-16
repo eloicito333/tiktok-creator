e37dd374b18ea7e8537e1426ac318e0925fa004b
Eloi Buil Cuadrat <eloixxl2008@gmail.com>
2025-04-16 02:04:32 +0200

    Remotion setup

---
+++ remotion/Root.jsx
@@ -0,0 +1,22 @@
+  // remotion/Root.tsx
+  import React from 'react';
+  import { Composition } from 'remotion';
+  import { TikTokVideo } from '#remotion/TikTokVideo.jsx';
+
+  export const RemotionRoot = ({title, processedScript}) => {
+    return (
+      <>
+        <Composition
+          id={title}
+          durationInFrames={150}
+          fps={30}
+          width={1080}
+          height={1920}
+          component={TikTokVideo}
+          defaultProps={{
+            processedScript,
+          }}
+        />
+      </>
+    );
+  };
---
+++ remotion/TikTokVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TikTokVideo({ processedScript }) {
+  return (
+    <div>TikTokVideo</div>
+  )
+}
+
+export default TikTokVideo
